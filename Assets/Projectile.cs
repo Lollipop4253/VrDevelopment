21a16e8500986e4c81db419b6cba8a28ea2c69f0
Me-Ri <egor.evdakimov@yandex.ru>
2025-10-26 17:54:24 +0500

    Test2

--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [Header("Impact Effects")]
-<<<<<<< HEAD
     [Tooltip("пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ (пїЅпїЅпїЅпїЅпїЅ, пїЅпїЅпїЅпїЅпїЅ пїЅ пїЅ.пїЅ.)")]
     public GameObject impactParticles; // пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
     public GameObject SmokeParticles;
@@ -11,26 +10,14 @@ public class Projectile : MonoBehaviour
     void Start(){
         Instantiate(SmokeParticles, transform.position, transform.rotation);
     }
-=======
-    [Tooltip("Префаб частиц при ударе (взрыв, искры и т.п.)")]
-    public GameObject impactParticles; // Префаб частиц
-
->>>>>>> dfedd42dd06e3f80c5104bdc3314a8ea8724a950
 
     void OnCollisionEnter(Collision collision)
     {
 
-<<<<<<< HEAD
         // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
         Vector3 contactPoint = collision.contacts[0].point;
 
         // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
-=======
-        // Получаем точку контакта
-        Vector3 contactPoint = collision.contacts[0].point;
-
-        // Воспроизводим частицы
->>>>>>> dfedd42dd06e3f80c5104bdc3314a8ea8724a950
         if (impactParticles != null)
         {
             Instantiate(impactParticles, transform.position, transform.rotation);
@@ -38,3 +25,4 @@ public class Projectile : MonoBehaviour
         Destroy(gameObject);
     }
 }
+//asd
